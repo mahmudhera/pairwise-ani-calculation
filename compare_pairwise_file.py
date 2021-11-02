13ab362b783184de1cac2fef61e202875df15bf1
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2021-11-01 22:02:03 -0400

    updated

--- compare_pairwise_file.py
+++ compare_pairwise_file.py
@@ -68,14 +68,14 @@ f = open(stats_filename, 'w')
 f.close()
 
 counts = [0]*10
-pairs = set()
+pairs = []
 
 for filename1 in glob.glob('data/*.fna'):
     for filename2 in glob.glob('data/*.fna'):
         if filename1 == filename2:
             continue
         if (filename2, filename1) not in pairs:
-            pairs.add( (filename1, filename2) )
+            pairs.append( (filename1, filename2) )
 
 print(len(pairs))
 
